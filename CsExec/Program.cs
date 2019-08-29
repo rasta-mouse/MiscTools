e251186490cba92161d3bd4e44ffe316212dce6c
Rasta Mouse <rasta@rastamouse.me>
2019-08-29 12:10:47 +0100

    CsExec

---
+++ CsExec/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading;
+
+using static CsExec.NativeMethods;
+
+namespace CsExec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length < 4)
+            {
+                Console.WriteLine(" [x] Invalid number of arguments");
+                Console.WriteLine("     Usage: CsExec.exe <targetMachine> <serviceName> <serviceDisplayName> <binPath>");
+                Environment.Exit(1);
+            }
+
+            string target = $@"\\{args[0]}";
+            string serviceName = args[1];
+            string serviceDisplayName = args[2];
+            string binPath = args[3];
+
+            try
+            {
+                // Connect to Service Manager
+                IntPtr hSCManager = OpenSCManager(target, null, SCM_ACCESS.SC_MANAGER_ALL_ACCESS);
+
+                if (hSCManager == IntPtr.Zero)
+                {
+                    Console.WriteLine($" [x] Could not open Service Manager on {target}");
+                    Environment.Exit(1);
+                }
+
+                // Create Service
+                IntPtr hService = CreateService(hSCManager, serviceName, serviceDisplayName, SERVICE_ACCESS.SERVICE_ALL_ACCESS, SERVICE_TYPE.SERVICE_WIN32_OWN_PROCESS, SERVICE_START.SERVICE_DEMAND_START, SERVICE_ERROR.SERVICE_ERROR_NORMAL, binPath, null, null, null, null, null);
+
+                if (hService == IntPtr.Zero)
+                {
+                    Console.WriteLine($" [x] Could not create service {serviceName}");
+                    Environment.Exit(1);
+                }
+
+                // Start Service
+                bool running = StartService(hService, 0, null);
+
+                Thread.Sleep(1000);
+
+                if (running)
+                {
+                    // Stop Service
+                    SERVICE_STATUS serviceStatus = new SERVICE_STATUS();
+                    bool stopped = ControlService(hService, SERVICE_CONTROL.STOP, ref serviceStatus);
+
+                    if (!stopped)
+                        Console.WriteLine($" [x] Could not stop service {serviceName}");
+                }
+                else
+                {
+                    Console.WriteLine($" [x] Could not start service {serviceName}");
+                }
+
+                Thread.Sleep(1000);
+
+                //Delete Service
+                bool deleted = DeleteService(hService);
+
+                if (!deleted)
+                    Console.WriteLine($" [x] Could not delete service {serviceName}");
+
+                // Close Handles
+                CloseServiceHandle(hService);
+                CloseServiceHandle(hSCManager);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# MiscTools
+
+## CsExec
+PsExec functionality in C#.
+
+### Usage
+`CsExec.exe <targetMachine> <serviceName> <serviceDisplayName> <binPath>`
+Also See [TikiService](https://rastamouse.me/2019/08/tikiservice/)
