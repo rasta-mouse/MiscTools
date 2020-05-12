a268bb402cf804e391458e7bb63644ac0027eca5
Rasta Mouse <rasta@rastamouse.me>
2020-05-12 19:59:31 +0100

    Update Program.cs

--- CsPosh/Program.cs
+++ CsPosh/Program.cs
@@ -5,6 +5,7 @@ using System.Management.Automation;
 using System.Management.Automation.Runspaces;
 
 using NDesk.Options;
+using System.Text;
 
 namespace CsPosh
 {
@@ -22,7 +23,7 @@ namespace CsPosh
             var outstring = false;
             var target = string.Empty;
             var code = string.Empty;
-            var encoded = string.Empty;
+            var encoded = false;
             var domain = string.Empty;
             var username = string.Empty;
             var password = string.Empty;
@@ -30,11 +31,11 @@ namespace CsPosh
             var options = new OptionSet(){
                 {"t|target=", "Target machine", o => target = o},
                 {"c|code=", "Code to execute", o => code = o},
-                {"e|encoded=", "Encoded Code to execute", o => encoded = o},
-                {"o|outstring", "Append Out-String to code", o => outstring = true },
-                {"d|domain=", "Domain for alternate credentials", o => domain = o },
-                {"u|username=", "Username for alternate credentials", o => username = o },
-                {"p|password=", "Password for alternate credentials", o => password = o },
+                {"e|encoded", "Indicates that provided code is base64 encoded", o => encoded = true},
+                {"o|outstring", "Append Out-String to code", o => outstring = true},
+                {"d|domain=", "Domain for alternate credentials", o => domain = o},
+                {"u|username=", "Username for alternate credentials", o => username = o},
+                {"p|password=", "Password for alternate credentials", o => password = o},
                 {"h|?|help","Show Help", o => help = true},
             };
 
@@ -48,12 +49,6 @@ namespace CsPosh
                     return;
                 }
                 
-                if (!string.IsNullOrEmpty(encoded))
-                {
-                    code = System.Text.ASCIIEncoding.ASCII.GetString(System.Convert.FromBase64String(encoded));
-                    Console.WriteLine("Encoded command to execute: " + code);
-                }
-
                 if (string.IsNullOrEmpty(target) || string.IsNullOrEmpty(code))
                 {
                     ShowHelp(options);
@@ -79,7 +74,6 @@ namespace CsPosh
                         pass.AppendChar(c);
 
                     var cred = new PSCredential($"{domain}\\{username}", pass);
-
                     conn.Credential = cred;
                 }
 
@@ -90,6 +84,7 @@ namespace CsPosh
                     using (var posh = PowerShell.Create())
                     {
                         posh.Runspace = runspace;
+                        if (encoded) { code = Encoding.Default.GetString(Convert.FromBase64String(code)).Replace("\0", ""); }
                         posh.AddScript(code);
                         if (outstring) { posh.AddCommand("Out-String"); }
                         var results = posh.Invoke();
